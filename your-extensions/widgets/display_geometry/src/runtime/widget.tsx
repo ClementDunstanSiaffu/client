294cbf146af9f1d7f28ca9d3c0902f2481228493
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2022-11-24 19:38:36 +0300

    placing DisplayGeometry in order

--- your-extensions/widgets/display_geometry/src/runtime/widget.tsx
+++ your-extensions/widgets/display_geometry/src/runtime/widget.tsx
@@ -9,17 +9,23 @@ export default class DisplayGeometry extends React.PureComponent<AllWidgetProps<
     }
 
    render(): React.ReactNode {
-        console.log(this.props.stateValue?.["id"])
         if(this.props.stateValue?.["id"]?.displayGeomtry){
             return (
-                <>
-                    {
-                        this.props.stateValue?.["id"]?.paths?.length > 0 ?
-                        this.props.stateValue?.["id"]?.paths.map((path)=>{
-                            return <div>{path}</div>
-                        }):null
-                    }
-                </>
+                <div style={{width:"100%",height:"100%",position:"absolute",top:0,bottom:0}}>
+                    <div style = {{marginLeft:"auto",marginRight:"auto"}}>
+                        <div style = {{fontSize:20,color:"grey"}}>Path coordinates</div>
+                        {
+                            this.props.stateValue?.["id"]?.paths?.length > 0 ?
+                            this.props.stateValue?.["id"]?.paths.map((path)=>{
+                                return (
+                                    <div style = {{marginTop:20,marginLeft:20}}>
+                                        {path}
+                                    </div>
+                                )
+                            }):null
+                        }
+                    </div>
+                </div>
             )
         }
        return null;
