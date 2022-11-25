ead8d99ac167db9d06cd9b3ee9bc78deb26eed50
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2022-11-25 16:19:04 +0300

    show only the active point

--- your-extensions/widgets/attribute_table/src/runtime/widget.tsx
+++ your-extensions/widgets/attribute_table/src/runtime/widget.tsx
@@ -19,25 +19,31 @@ export default class AttributeTable extends React.PureComponent<AllWidgetProps<a
     }
 
     render(){
-        console.log(this.props.stateValue?.["id"]?.paths)
+        let xCoorninates = this.props.stateValue?.["id"]?.paths[this.props.stateValue?.["id"]?.paths.length-1][0]??0
+        let yCoordinates = this.props.stateValue?.["id"]?.paths[this.props.stateValue?.["id"]?.paths.length-1][1]??0
         return(
             <Table hover>
             <thead>
               <tr>
-                <th>#</th>
                 <th>Path(x,y)</th>
               </tr>
             </thead>
             <tbody>
-              {
+            {
+                <tr>
+                    <td>({xCoorninates},{yCoordinates})</td>
+                  </tr>
+                
+              }
+              {/* {
                 this.props.stateValue?.["id"]?.paths?.length > 0 ?
-                this.props.stateValue?.["id"]?.paths.map((path,k)=>(
+                this.props.stateValue?.["id"]?.paths[this.props.stateValue?.["id"]?.paths.length-1].map((path,k)=>(
                   <tr key = {k}>
                     <td>{k+1}</td>
                     <td>({path[0]},{path[1]})</td>
                   </tr>
                 )):null
-              }
+              } */}
             </tbody>
           </Table>
         )
