23c3a4fb7d9ed9ddc4e60b9f8ee0c74cd9da1426
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2022-11-30 18:23:39 +0300

    remove console.log

--- your-extensions/widgets/creative_widget/src/components/themesComponents/darkTheme.tsx
+++ your-extensions/widgets/creative_widget/src/components/themesComponents/darkTheme.tsx
@@ -5,7 +5,6 @@ import '../../assets/css/app-creative-dark.min.css';
 export class DarkThemeComponent extends React.PureComponent<any,any>{
 
     render(): React.ReactNode {
-        console.log("dark is called ")
         return null;
     }
 }
--- your-extensions/widgets/creative_widget/src/components/themesComponents/lightTheme.tsx
+++ your-extensions/widgets/creative_widget/src/components/themesComponents/lightTheme.tsx
@@ -3,8 +3,6 @@
 import {React,jsx} from 'jimu-core';
 import '../../assets/css/app-creative.min.css';
 
-console.log("light is called ")
-
 export class LightThemeComponent extends React.PureComponent<any,any>{
 
     render(): React.ReactNode {
