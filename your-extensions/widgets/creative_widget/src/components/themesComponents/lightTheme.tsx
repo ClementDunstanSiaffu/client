8c990d8afd8607fa6f447d827bc30d6312cd3f6d
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2022-11-30 18:24:33 +0300

    remove console.log in the light theme component

--- your-extensions/widgets/creative_widget/src/components/themesComponents/lightTheme.tsx
+++ your-extensions/widgets/creative_widget/src/components/themesComponents/lightTheme.tsx
@@ -6,7 +6,6 @@ import '../../assets/css/app-creative.min.css';
 export class LightThemeComponent extends React.PureComponent<any,any>{
 
     render(): React.ReactNode {
-        console.log("light is called ")
         return null;
     }
 }
